1f3bc48b95f980735e8e08da4c551c0cbb3ca01b
Christopher Dang <christhd@uci.edu>
2020-11-26 21:00:08 -0800

    Server parity to client

---
+++ Assets/Scripts/DoorCloser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCloser : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+        }
+    }
+
+    private void closeDoor()
+    {
+        Door.GetComponent<Transform>().Rotate(0, 0, -90, Space.Self);
+        Door.GetComponent<BoxCollider>().enabled = true;
+    }
+}
